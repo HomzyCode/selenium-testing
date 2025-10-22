c4d927b6aac07ddccacc85f0ad41ac0c99edd58b
HomzyCode <marcusfadairo777@gmail.com>
2025-10-21 22:24:57 -0700

    access and interact with element

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 import time
 
 # Use full path to chromedriver (works for Apple Silicon)
@@ -8,6 +9,11 @@ service = Service(executable_path="/opt/homebrew/bin/chromedriver")
 driver = webdriver.Chrome(service=service)
 
 driver.get("https://google.com")
+
+# find search field, type, and enter
+# will look for the element specified by the class name to access and interact
+input_element = driver.find_element(By.CLASS_NAME, "gLFyf")
+input_element.send_keys("tech with tim")
 
 time.sleep(10)
 
