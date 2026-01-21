580adde4b6029d4d6760cf1dc77b4b9631ac98bb
Marcus Fadairo <marcusf.@Mac.lan>
2026-01-21 02:52:24 -0800

    more stuff

--- main.py
+++ main.py
@@ -2,15 +2,21 @@ from selenium import webdriver
 # from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 # Use full path to chromedriver (works for Apple Silicon)
 driver = webdriver.Chrome()
 driver.get("https://google.com")
 
-# find search field, type, and enter
-# will look for the element specified by the class name to access and interact
+
+WebDriverWait(driver, 5).until(
+    EC.presence_of_element_located((By.CLASS_NAME, "gLFyf"))
+)
+
 input_element = driver.find_element(By.CLASS_NAME, "gLFyf")
+input_element.clear()
 input_element.send_keys("tech with tim" + Keys.ENTER)
 
 # test test 
